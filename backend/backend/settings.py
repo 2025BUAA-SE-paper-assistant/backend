08967ba18604462a22dbbf266faa7348237ca8ea
郑广懿 <22371212@buaa.edu.cn>
2025-04-14 15:07:41 +0800

    feat:尝试部署ai功能

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -207,7 +207,7 @@ MAX_Similarity = 0.8  # 最大相似度，介于-1和1之间，不确定
 # 远程模型部署开放的API接口
 REMOTE_MODEL_BASE_PATH = "10.2.16.28:2334"
 # 使用openai流式接口调用glm3大模型，不附带知识库
-REMOTE_CHATCHAT_GLM3_OPENAI_PATH = "172.17.62.88:20005"
+REMOTE_CHATCHAT_GLM3_OPENAI_PATH = "10.2.16.28:2338"
 
 # 语义检索相关
 VECTOR_DIM = 1024
