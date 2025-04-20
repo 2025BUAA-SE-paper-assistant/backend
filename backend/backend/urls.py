23de22a6db965d6375ee0fd07f89ec2311eeb8b2
Zhuhao Wu <3227038208@qq.com>
2025-04-20 09:12:11 +0800

    路由挂载

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -72,6 +72,7 @@ from business.api.summary import (
     create_abstract_report,
     get_summary_status,
 )
+from business.api.translate import translate_text
 
 from business.api.paper_recommend import get_recommendation
 from business.api.remark_api import (
@@ -179,5 +180,5 @@ urlpatterns = [
     path("api/remark/delete/<int:remark_id>", delete_remark),
     path("api/remark/like/<int:remark_id>", like_remark),
     # 翻译
-    path("api/translate",),
+    path("api/translate", translate_text),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
