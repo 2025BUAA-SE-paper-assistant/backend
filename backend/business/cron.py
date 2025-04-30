5690407cbdded465953e075fab9402016f1ba103
Zhuhao Wu <3227038208@qq.com>
2025-04-30 09:52:57 +0800

    fix:仅为过去三天活跃的用户设置个性化推荐缓存

--- backend/business/cron.py
+++ backend/business/cron.py
@@ -1,11 +1,21 @@
 '''定时任务'''
 from business.utils.recommend import refresh_personal_recommend_cache
 from business.models import User
+from business.models.statistic import UserActivityStat
+from django.utils import timezone
+from datetime import timedelta
 import logging
 logger = logging.getLogger('business')
 def refresh_all_personal_recommend_cache():
     '''个性化推荐缓存定时任务'''
-    users = User.objects.all()
+    # users = User.objects.all()
+    # 仅为过去三天的活跃用户设置缓存
+    end_time = timezone.now()
+    start_time = end_time - timedelta(days=3)
+    user_ids = list(UserActivityStat.objects.filter(
+        timestamp__range=(start_time, end_time)
+    ).values_list('user_id', flat=True).distinct())
+    users = User.objects.filter(user_id__in=user_ids).all()
     max_retries = 3  # 最大重试次数
     for user in users:
         for attempt in range(max_retries):
