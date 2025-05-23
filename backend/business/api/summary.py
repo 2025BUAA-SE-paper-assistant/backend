1547ffc927a4fc2e5fcfcdc919be165aabdf5f6e
zjq <894618229@qq.com>
2025-05-23 17:39:19 +0800

    优化综述结论生成部分

--- backend/business/api/summary.py
+++ backend/business/api/summary.py
@@ -198,7 +198,7 @@ def get_summary(paper_ids, report_id, user):
             "temperature": 0.2, # temp
             "stream": False, 
             "model_name": "chatglm3-6b", # 模型
-            "max_tokens": 4096,
+            "max_tokens": 8192,
             "prompt_name": "tech_innovation_analyzer", # prompt类型
         }
 
@@ -232,7 +232,7 @@ def get_summary(paper_ids, report_id, user):
             "temperature": 0.3, # temp
             "stream": False, 
             "model_name": "chatglm3-6b", # 模型
-            "max_tokens": 4096,
+            "max_tokens": 8192,
             "prompt_name": "performance_analyzer", # prompt类型
         }
 
@@ -258,7 +258,39 @@ def get_summary(paper_ids, report_id, user):
                 # 从匹配行开始，拼接后续所有行
                 limit = '\n'.join(lines[idx:]).strip()
 
-        # limit = "## " + limit     
+        print("结论生成开始")
+        data = {
+            "query": f"{articles}", # 原文
+            "temperature": 0.3, # temp
+            "stream": False, 
+            "model_name": "chatglm3-6b", # 模型
+            "max_tokens": 8192,
+            "prompt_name": "future_vision_generator", # prompt类型
+        }
+
+        payload = json.dumps(data)
+
+        response = requests.post(f"{base_url}/chat", data=payload, headers=headers, stream=False)
+        ans = ""
+        # 捕获输出
+        for line in response.iter_lines():
+            decoded_line = line.decode('utf-8')
+            if decoded_line.startswith(': ping'):  # 忽略以 ":" 开头的行
+                continue
+        # print(decoded_line)
+            if decoded_line.startswith('data'):
+                data = json.loads(decoded_line.replace('data: ', ''))
+                ans += data['text']
+
+        lines = ans.splitlines()
+        conclusions = ""
+        for idx, line in enumerate(lines):
+            if line.strip() == '# 研究结论与未来展望':
+                # 从匹配行开始，拼接后续所有行
+                conclusions = '\n'.join(lines[idx:]).strip()
+
+        # 捕获所有问题的内容
+        print("研究结论与未来展望结束")     
                 
                  
         # 生成综述
@@ -273,7 +305,10 @@ def get_summary(paper_ids, report_id, user):
         if limit != "" :
             limit = "## " + limit
             summary += limit + "\n"
-        summary += "\n# 结论\n" + conclusion + "\n"
+        if conclusions != "" :
+            summary += "\n" + conclusions + "\n"
+        else :
+            summary += "\n# 结论\n" + conclusion + "\n"
         # 修改语病，更加通顺
         response = summary
         os.makedirs(os.path.dirname(settings.USER_REPORTS_PATH), exist_ok=True)
